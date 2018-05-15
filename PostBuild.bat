f014122a68f1fd8aff44757d5452b870ecd3d8b7
afozil <af1142@hotmail.com>
2018-05-15 06:34:28 +0300

    Committng Unit Tests  - 15-May-2018

---
+++ PostBuild.bat
@@ -0,0 +1,4 @@
+if exist "%WORKSPACE%\TestResults.trx" (
+del "%WORKSPACE%\TestResults.trx" 
+)
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\MSTest.exe" /resultsfile:"%WORKSPACE%\TestResults.trx" /testcontainer:"C:\Projects\HotelReservationSystemUnitTest\bin\Debug\HotelReservationSystemUnitTest.dll" /nologo
