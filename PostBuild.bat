105f221f2cb0c846ba86f0ecf3891ec354f8acc6
afozil <af1142@hotmail.com>
2018-05-19 11:12:19 +0300

    .bat

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,4 +1,6 @@
 if exist "%WORKSPACE%\TestResults.trx" (
 del "%WORKSPACE%\TestResults.trx" 
 )
+echo %WORKSPACE%
+
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\MSTest.exe" /resultsfile:"%WORKSPACE%\TestResults.trx" /testcontainer:"C:\Projects\HotelReservationSystemUnitTest\bin\Debug\HotelReservationSystemUnitTest.dll" /nologo
